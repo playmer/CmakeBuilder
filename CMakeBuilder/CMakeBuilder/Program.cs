9072d3cea1f001fdd1783da892aa1df578c77af4
Joshua T. Fisher <playmer@gmail.com>
2018-01-16 15:07:50 -0800

    Work

--- CMakeBuilder/CMakeBuilder/Program.cs
+++ CMakeBuilder/CMakeBuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,54 +8,95 @@ namespace CMakeBuilder
 {
   class Program
   {
-    static void Main(string[] args)
+    static void DoDirectory(string aDirectory)
     {
-      var directories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+      var sourceTypes = new SortedSet<string>();
+      sourceTypes.Add(".cpp");
+      sourceTypes.Add(".cc");
+      sourceTypes.Add(".c");
 
-      string workingDirectory = Path.GetFullPath(Directory.GetCurrentDirectory()).TrimEnd(Path.DirectorySeparatorChar);
+      var headerTypes = new SortedSet<string>();
+      headerTypes.Add(".hpp");
+      headerTypes.Add(".hh");
+      headerTypes.Add(".h");
+      headerTypes.Add(".inl");
+
+      var directories = Directory.GetDirectories(aDirectory);
+
+      string workingDirectory = Path.GetFullPath(aDirectory).TrimEnd(Path.DirectorySeparatorChar);
       string workingDirectoryName = workingDirectory.Split(Path.DirectorySeparatorChar).Last();
-
-      string projectSource = string.Concat(workingDirectoryName, "_Source");
-      string oldProjectSource = string.Concat("Previous_", workingDirectoryName, "_Source");
 
       foreach (var directory in directories)
       {
+        var privateSources = new List<string>();
+        var publicSources = new List<string>();
         string directoryName = Path.GetDirectoryName(directory);
 
         StringBuilder system = new StringBuilder();
 
-        system.Append("Set(");
-        system.Append(oldProjectSource);
-        system.Append(" ${");
-        system.Append(projectSource);
-        system.Append("})\n\n");
+        system.Append("target_sources(");
+        system.Append(workingDirectoryName);
+        system.Append("\n");
 
         string[] files = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
 
         foreach (var file in files)
         {
+          var relativeFile = Path.GetRelativePath(directory, file).Replace('\\', '/');
           string fileName = Path.GetFileName(file);
 
-          system.Append("AddFile(");
-          system.Append(projectSource);
-          system.Append(" ");
-          system.Append(oldProjectSource);
-          system.Append(" ");
-          system.Append(fileName);
-          system.Append(")\n");
+          var extension = Path.GetExtension(fileName);
+
+          if (sourceTypes.Contains(extension))
+          {
+            privateSources.Add(relativeFile);
+          }
+          else if (headerTypes.Contains(extension))
+          {
+            publicSources.Add(relativeFile);
+          }
         }
 
-        system.Append("\nSet(");
-        system.Append(projectSource);
-        system.Append(" ${");
-        system.Append(oldProjectSource);
-        system.Append("} PARENT_SCOPE)\n");
+        system.Append("  PRIVATE\n");
+
+        foreach (var file in privateSources)
+        {
+          system.Append("    ${CMAKE_CURRENT_LIST_DIR}/");
+          system.Append(file);
+          system.Append("\n");
+        }
+
+        system.Append("#  PUBLIC\n");
+
+        foreach (var file in publicSources)
+        {
+          system.Append("    ${CMAKE_CURRENT_LIST_DIR}/");
+          system.Append(file);
+          system.Append("\n");
+        }
+
+        system.Append(")\n");
 
         Console.WriteLine(system.ToString());
-        //System.IO.File.WriteAllText("CMakeLists.txt", system.ToString());
+
+        if (0 == publicSources.Count && 0 == privateSources.Count)
+        {
+          System.IO.File.WriteAllText(Path.Combine(directory, "CMakeLists.txt"), "\n");
+        }
+        else
+        {
+          System.IO.File.WriteAllText(Path.Combine(directory, "CMakeLists.txt"), system.ToString());
+        }
       }
 
       //System.IO.File.WriteAllText("CMakeLists.txt", "");
+    }
+
+    static void Main(string[] args)
+    {
+      DoDirectory("C:/Users/playm/Documents/GitKraken/LambPlanet/Dependencies/YTE/Dependencies/assimp");
+      DoDirectory("C:/Users/playm/Documents/GitKraken/LambPlanet/Dependencies/YTE/Dependencies/Bullet");
+      //DoDirectory("C:/Users/playm/Documents/GitKraken/LambPlanet/Dependencies/YTE/Dependencies/LuaJIT");
     }
   }
 }
